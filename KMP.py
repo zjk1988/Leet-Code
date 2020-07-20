f6422594468bf9c5e7136ec7ab30fc47b014d762
zjk1988 <53870942+zjk1988@users.noreply.github.com>
2020-07-20 15:08:55 +0800

    Add files via upload

--- KMP.py
+++ KMP.py
@@ -1,35 +1,38 @@
 参考：
 https://www.cnblogs.com/yjiyjige/p/3263858.html
 https://blog.csdn.net/your_answer/article/details/79619406
-
-def KMP(L,t):
+上面两个博客稍微有点错误，就是不匹配的时候会出不去循环
+def KMP(s1,s2):
+    next_arr = next_array(s2)
     i = 0
     j = 0
-    nexts = getnext(t)
-    while (i<len(L) and j<len(t)):
-        if L[i]==t[j] or j==-1:
-            i = i+1
-            j = j+1
+    l1 = len(s1)
+    l2 = len(s2)
+    while (i<l1 and j<l2):
+        if j==-1 or s1[i]==s2[j]:
+            
+            j+=1
         else:
-            j = nexts[j]
-    if j == len(t):
+            j = next_arr[j]
+        i+=1
+    if j == l2:
         return i-j
     else:
         return -1
-def getnext(t):
-    j = 0
-    k = 0
-    nexts = [0 for _ in range(len(t))]
+def next_array(s):
+    l = len(s)
+    nexts = [0 for _ in range(l)]
     nexts[0] = -1
-    for indx in range(len(t)-1) :#记得-1因为记录的是刚刚不匹配的点的
-        if indx==-1 or nexts[j]==nexts[k]:
-            j = j+1
+    j=0
+    k=0
+    while j<l:
+        if (k==-1 or nexts[j]==nexts[k]):
             k = k+1
+            j = j+1
             nexts[j] = k
         else:
             k = nexts[k]
+        j = j+1
     return nexts
-        
-KMP('12343232','343')        
-    
-getnext('aaaaas')    
+KMP("hfllo",'ll')
+ 
